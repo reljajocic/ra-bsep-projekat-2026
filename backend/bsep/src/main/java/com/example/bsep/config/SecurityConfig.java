c2b26b6d62aecc3778b6d6ee8c347511537d1e87
Relja Jocić <149072501+reljajocic@users.noreply.github.com>
2026-06-09 18:07:36 +0200

    backend functional, some endpoints tested

--- backend/bsep/src/main/java/com/example/bsep/config/SecurityConfig.java
+++ backend/bsep/src/main/java/com/example/bsep/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -43,6 +45,13 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            throw new UsernameNotFoundException("Use JWT authentication");
+        };
     }
 
     @Bean
